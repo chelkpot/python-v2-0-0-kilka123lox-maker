1cec93705c032881a98860c4192bc8109faf7ce2
student <student@corp.kpiot.ru>
2025-10-23 14:05:23 +0500

    фаф

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a,b = map(int, input().split())
+    total = a + b - 1
+    print(total - a, total - b)-a ,b = map(int, input().split())
+    total = a + b - 1
+    print(total - a, total - b)  
 
 
 # Код ниже не трогать! он нужен для тестов
